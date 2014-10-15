14e5581cacfe8fb5081619ce55db35077c0897c2
云巅 <mcloudtop@gmail.com>
2014-10-15 23:05:50 +0800

    2.3

--- noteweb/WebRoot/scripts/notebook/alert_addnotebook.js
+++ noteweb/WebRoot/scripts/notebook/alert_addnotebook.js
@@ -1,58 +1,49 @@
 $(function(){
 	$.ajax({
 		type:"get",
-		url:"http://localhost:8080/testNoteServer/NoteBook/findAllNoteBook",
+		url:"http://localhost:8080/noteserver/NoteBook/findAllNoteBook",
 		success:function(data){
 			if(data!=""){	
 				var list = data.data;
-				for(var i=0;i<list.length;i++){					
+				for(var i=0;i<list.length;i++){
+					
 					$("#lists").append(
-							"<li class='online'><a ><i class='fa fa-book' title='online'rel='tooltip-bottom'></i>"+list[i].notebookName+"</a></li>"		
+							"<li class='notebookonline' id='"+list[i].notebookId+"'><a ><i class='fa fa-book' title='online'rel='tooltip-bottom'></i>"+list[i].notebookName+"</a><button type='button'   class='sd' id='add_notebook'><i class='fa fa-times'></i></button></li>"		
 							);
-				};
-				$("li").dblclick(function(){
-					var prename = $(this).text();
-					$("#can").load("alert/alert_rename.html",function(){
-						$("#can").show();
-						$(".close").click(function(){
-							$("#can").hide();
-						});
-						$("#cancle").click(function(){
-							$("#can").hide();
-						});
-						$("#updata").click(function(){
-							var newname = $("#input_notebook_rename").val();
-							$.ajax({
-								url:"http://localhost:8080/testNoteServer/NoteBook/updataNoteBookName",
-								type:"post",
-								data:{"prename":prename,"newname":newname},
-								success:function(data){
-										$("#can").hide();
-										//遍历数据库中的笔记本
-										$("#lists").html("<li class='online'><a  class='checked'><i class='fa fa-book' title='online' rel='tooltip-bottom'></i> 默认笔记本</a></li>");
-										$.ajax({
-											type:"get",
-											url:"http://localhost:8080/testNoteServer/NoteBook/findAllNoteBook",
-											success:function(data){
-												if(data!=""){	
-													var list = data.data;
-													for(var i=0;i<list.length;i++){					
-														$("#lists").append(
-																"<li class='online'><a ><i class='fa fa-book' title='online'rel='tooltip-bottom'></i>"+list[i].notebookName+"</a></li>"
-																);
-													}	
-													}
-											}
-										});
-								}
+					$("li").dblclick(function(){
+						var notebookId = $(this).attr("id");
+						$("#can").load("alert/alert_rename.html",function(){
+							$("#can").show();
+							$(".close").click(function(){
+								$("#can").hide();
 							});
-							
-						})
+							$("#cancle").click(function(){
+								$("#can").hide();
+							});
+							$("#updata").click(function(){
+								var newname = $("#input_notebook_rename").val();
+								$.ajax({
+									url:"http://localhost:8080/noteserver/NoteBook/updataNoteBookName",
+									type:"post",
+									data:{"notebookId":notebookId,"newname":newname},
+									success:function(data){
+											$("#can").hide();
+											window.location.href="edit.html";
+									}
+								});
+								
+							});
+						});
 					});
+					
+				};
+				$(".sd").click(function(){
+					$("#can").load("alert/alert_delete_notebook.html");
 				});
 			};
 		}
 	});
+	
 	$("#add_notebook").click(function(){
 		$("#can").load("alert/alert_notebook.html",function(){
 			$("#can").show();
@@ -72,7 +63,9 @@ $(function(){
 		
 	});
 	
+	
+	
 
 	
-})
+});
 
